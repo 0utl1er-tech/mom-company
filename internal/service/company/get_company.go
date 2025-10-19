45bdbc591348cca0bbadbc6c928d6d21c164cef4
bitcat <kurohaj96@gmail.com>
2025-10-19 21:13:28 +0900

    chore: bufの設定を変更

--- internal/service/company/get_company.go
+++ internal/service/company/get_company.go
@@ -5,6 +5,7 @@ import (
 
 	companyv1 "github.com/0utl1er-tech/mom-company/gen/pb/company/v1"
 	contactv1 "github.com/0utl1er-tech/mom-company/gen/pb/contact/v1"
+	staffv1 "github.com/0utl1er-tech/mom-company/gen/pb/staff/v1"
 	"github.com/0utl1er-tech/mom-company/internal/util"
 	"github.com/google/uuid"
 	"google.golang.org/protobuf/types/known/timestamppb"
@@ -26,11 +27,30 @@ func (s *Service) GetCompany(ctx context.Context, req *companyv1.GetCompanyReque
 		return nil, err
 	}
 
+	staffs, err := s.db.ListStaff(ctx, companyID)
+	if err != nil {
+		return nil, err
+	}
+
 	// レスポンス用のContactを作成
 	contact := &contactv1.Contact{
 		Id:    company.ContactID.String(),
 		Email: company.ContactEmail.String,
 		Phone: company.ContactPhone.String,
+	}
+
+	staffList := []*staffv1.Staff{}
+	for _, staff := range staffs {
+		staffList = append(staffList, &staffv1.Staff{
+			Id:   staff.ID.String(),
+			Name: staff.Name,
+			Role: staff.Role,
+			Contact: &contactv1.Contact{
+				Id:    staff.ContactID.String(),
+				Email: staff.ContactEmail.String,
+				Phone: staff.ContactPhone.String,
+			},
+		})
 	}
 
 	return &companyv1.GetCompanyResponse{
@@ -42,6 +62,7 @@ func (s *Service) GetCompany(ctx context.Context, req *companyv1.GetCompanyReque
 			Address:     company.Address,
 			CompanyCode: company.CompanyCode,
 			Contact:     contact,
+			Staff:       staffList,
 			CreatedAt:   timestamppb.New(company.CreatedAt),
 		},
 	}, nil
